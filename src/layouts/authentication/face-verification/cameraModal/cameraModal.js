460a840d17d1eb101ac429d183a35100ca0d1525
Sooraj Prasad <soorajprasad@zdistancelab.com>
2024-08-12 21:30:22 +0530

    mn under construction cameramodal.js

--- src/layouts/authentication/face-verification/cameraModal/cameraModal.js
+++ src/layouts/authentication/face-verification/cameraModal/cameraModal.js
@@ -1,4 +1,5 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
 import PropTypes from "prop-types";
 import Webcam from "react-webcam";
 import * as faceapi from "face-api.js";
@@ -6,17 +7,44 @@ import "./cameraModal.css";
 
 export const CameraModal = ({ isOpen, onClose }) => {
   const webcamRef = useRef();
+  const [message, setFacenetMessage] = useState("Place the face in the oval.");
+  const [outline, setOutline] = useState("#f00000");
+  let intervalId,screenShot
+
+  const takeScreenshot = () => {
+    console.log("Screenshot took");
+    screenShot = webcamRef.current.getScreenshot()
+    toast("Image added")
+    onClose()
+  };
+
+  useEffect(() => {
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [isOpen]);
 
   const handleStreamVideo = async (e) => {
     await faceapi.nets.tinyFaceDetector.loadFromUri("/facenet/models/tiny_face_detector");
-    const intervalId = setInterval(async () => {
-      const faces = await faceapi.detectAllFaces(
-        webcamRef.current.video,
-        new faceapi.TinyFaceDetectorOptions()
-      );
-      if (faces) {
-        console.log(faces);
+    let counter = 0;
+    intervalId = setInterval(async () => {
+      if (counter <= 40) {
+        const faces = await faceapi.detectAllFaces(
+          webcamRef.current.video,
+          new faceapi.TinyFaceDetectorOptions()
+        );
+        if (faces.length === 1 && faces[0].score > 0.5) {
+          counter++;
+          setOutline("#00ff00");
+          setFacenetMessage("Stand still for " + Math.round(4 - counter / 10) + " seconds.");
+        } else {
+          counter = 0;
+          setOutline("#f00000");
+          setFacenetMessage("Place the face in the oval");
+        }
+      } else {
         clearInterval(intervalId);
+        takeScreenshot();
       }
     }, 100);
   };
@@ -28,18 +56,23 @@ export const CameraModal = ({ isOpen, onClose }) => {
         <span className="modalClose" onClick={onClose}>
           &times;
         </span>
-        <Webcam
-          id="webcam"
-          className="camera-video"
-          ref={webcamRef}
-          screenshotFormat="image/jpeg"
-          screenshotQuality={1}
-          mirrored={true}
-          width={570}
-          height={700}
-          videoConstraints={{ facingMode: "user" }}
-          onUserMedia={(e) => handleStreamVideo(e)}
-        />
+        <div className="camera-and-overlay-holder">
+          <Webcam
+            id="webcam"
+            className="camera-video"
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            screenshotQuality={1}
+            mirrored={true}
+            width={570}
+            height={700}
+            videoConstraints={{ facingMode: "user" }}
+            onUserMedia={(e) => handleStreamVideo(e)}
+          ></Webcam>
+          <div className="camera-face-overlay" style={{ borderColor: outline }}>
+            <div className="camera-face-message">{message}</div>
+          </div>
+        </div>
       </div>
     </div>
   );
