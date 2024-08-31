b0a7b4a751ae95f245f45fe29a9b92a46f9b962e
Sooraj Prasad <soorajprasad@zdistancelab.com>
2024-09-01 03:35:02 +0530

    api link changed

--- src/services/common/registerFace.js
+++ src/services/common/registerFace.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const registerFaceService = async (data) => {
   try {
-    const response = await axios.post("http://localhost:5000/common/verify-link", data);
+    const response = await axios.post("http://localhost:5000/common/register-face", data);
     return response.data;
   } catch (error) {
     throw error.response.data
