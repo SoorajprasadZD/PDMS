374bcb3d7f4d6953f5917779e30d531c9c189d0a
Sooraj Prasad <soorajprasad@zdistancelab.com>
2024-09-01 20:52:19 +0530

    generate pdf api change

--- src/services/common/fetchReportPdf.js
+++ src/services/common/fetchReportPdf.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchReportPdf = async (data) => {
   try {
-    const response = await axios.post("http://localhost:5000/patient/generate-report-pdf", data, {
+    const response = await axios.post("http://localhost:5000/patients/generate-report-pdf", data, {
       responseType: "blob",
     });
     return response.data;
