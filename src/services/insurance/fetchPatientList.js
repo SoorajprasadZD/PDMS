c92342f9b22e8e4522dc53f48d07b5ce572f54e9
Adarsh V S <adarsh@zdistancelab.com>
2024-09-02 20:52:47 +0530

    fet

--- src/services/insurance/fetchPatientList.js
+++ src/services/insurance/fetchPatientList.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export const fetchPatientReports = async (patientId,insuranceCompanyIdAsSender) => {
-    try {
-      const response = await axios.get(`http://localhost:5000/common/fetch-patient-reports?patientId=${patientId}&insuranceCompanyIdAsSender=${insuranceCompanyIdAsSender}`);
-      return response.data;
-    } catch (error) {
-      throw error.response.data;
-    }
+  try {
+    const response = await axios.get(`http://localhost:5000/patients/${patientId}/report`);
+    return response.data.data;
+  } catch (error) {
+    throw error.response.data;
+  }
   };
