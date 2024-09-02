5d4dba631f7ba508cffe6e2dc12f787dc3027698
Adarsh V S <adarsh@zdistancelab.com>
2024-09-02 20:16:09 +0530

    fetch all hospitals

--- src/services/admin/fetchAllHospitals.js
+++ src/services/admin/fetchAllHospitals.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export const fetchAllHospitals = async () => {
     try {
-      const response = await axios.get("http://localhost:5000/admin/fetch-all-hospitals");
-      return response.data;
+      const response = await axios.get("http://localhost:5000/doctors/");
+      return response.data.data;
     } catch (error) {
       throw error.response.data;
     }
